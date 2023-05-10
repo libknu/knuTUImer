1f2454f9112b15a07387dbbe391cdd0001d290f5
Jiwoo Ahn <ilp_rincipe@naver.com>
2023-05-10 15:07:32 +0900

    Feat: x macro for err detection

--- includes/server.h
+++ includes/server.h
@@ -1,5 +1,10 @@
 #ifndef __SERVER_HPP__
-#define __SERVER_HPP__
+# define __SERVER_HPP__
+
+# include <sys/select.h>
+
+# define Xv(err,res,str)	(x_void(err,res,str,__FILE__,__LINE__))
+# define X(err,res,str)		(x_int(err,res,str,__FILE__,__LINE__))
 
 typedef struct s_server
 {
