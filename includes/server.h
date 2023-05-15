356434f8b86e8c3062ef99ede5c8b4368a2a3acf
Jiwoo Ahn <ilp_rincipe@naver.com>
2023-05-15 20:29:02 +0900

    Feat: loop for the server

--- includes/server.h
+++ includes/server.h
@@ -6,6 +6,8 @@
 # define Xv(err,res,str)	(x_void(err,res,str,__FILE__,__LINE__))
 # define X(err,res,str)		(x_int(err,res,str,__FILE__,__LINE__))
 
+# define MAX(a,b)	((a > b) ? a : b)
+
 # define BUF_SIZE	4096
 
 # define FD_FREE	0
--- server/loop.c
+++ server/loop.c
@@ -1,5 +1,43 @@
+#include <sys/select.h>
+#include <string.h>
 #include "server.h"
+
+void	init_fd(t_server *server)
+{
+	int	i;
+
+	i = 0;
+	server->max = 0;
+	FD_ZERO(&server->fd_read);
+	FD_ZERO(&server->fd_write);
+	while (i < server->maxfd)
+	{
+		if (server->fds[i].type != FD_FREE)
+		{
+			FD_SET(i, &server->fd_read);
+			if (strlen(server->fds[i].buf_write) > 0)
+				FD_SET(i, &server->fd_write);
+			server->max = MAX(server->max, i);
+		}
+		i++;
+	}
+}
 
 void	loop(t_server* server)
 {
+	while (1)
+	{
+		init_fd(server);	
+		server->r = select(server->max + 1, &server->fd_read, &server->fd_write, NULL, NULL);
+		for (int i = 0; (i < server->maxfd) && (server->r > 0); ++i)
+		{
+			if (FD_ISSET(i, &server->fd_read))
+				server->fds[i].fct_read(server, i);
+			else if (FD_ISSET(i, &server->fd_write))
+				server->fds[i].fct_write(server, i);
+			else
+				continue;
+			server->r--;
+		}
+	}
 }
