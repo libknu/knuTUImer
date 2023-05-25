83010ca2426d6cfd6b73af258931b235fb7a1b9a
Jiwoo Ahn <ilp_rincipe@naver.com>
2023-05-25 15:06:44 +0900

    Feat: parse group file

--- server/parsefile.c
+++ server/parsefile.c
@@ -52,6 +52,50 @@ static void getusersinfo(t_server *server)
 
 static void getgroupsinfo(t_server *server)
 {
+	int		op_flag;
+	ssize_t	linesize;
+	char	*token;
+	char	*buffer = (char *)malloc(sizeof(char) * BUFSIZ);
+	FILE*	fp = fopen(GROUPPATH, "r");
+
+	while (1)
+	{
+		linesize = getdelim(&buffer, 0, '\n',  fp);
+		if (linesize == -1)
+			break;
+		t_group *newgroup = (t_group *)malloc(sizeof(t_group));
+		token = strtok(buffer, ",");
+		newgroup->name = (char *)malloc(sizeof(char) * strlen(token));
+		strcpy(newgroup->name, token);
+		token = strtok(NULL, ",");
+		newgroup->total_elapsed = atoi(token);
+
+		linesize = getdelim(&buffer, 0, '\n',  fp);
+		token = strtok(buffer, ",");
+		op_flag = 0;
+		while (token != NULL)
+		{
+			t_list *node = server->users;
+			while (node != NULL)
+			{
+				if (strcmp(((t_user *)node->content)->id, token) == 0)
+				{
+					if (op_flag == 0)
+					{
+						lstadd_back(&(newgroup->op_user), lstnew(node->content));
+						op_flag = 1;
+					}
+					else
+						lstadd_back(&(newgroup->joined_users), lstnew(node->content));
+					break;
+				}
+				node = node->next;
+			}
+			token = strtok(NULL, ",");
+		}
+		lstadd_back(&(server->groups), lstnew(newgroup));
+	}
+	fclose(fp);
 }
 
 void	readfromfile(t_server *server)
